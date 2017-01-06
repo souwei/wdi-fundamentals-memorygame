3597e48958557686fc2999b1851a65698d4aaa6c
souwei <sou.wei.wong@gmail.com>
2017-01-06 16:42:30 +0800

    Added Card Generator in JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,20 @@
 console.log("JS file is connected to HTML! Woo!")
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour= "king";
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour= "king";
+//if (cardTwo === cardFour){
+//	alert('You found a match!');
+//}
 
-if (cardTwo === cardFour){
-	alert('You found a match!');
+
+function generateCards(){
+var board = document.getElementById('game-board');
+var numberOfCards=4;
+for(var count=0; count< numberOfCards;count++){
+	var carde = document.createElement('div');
+	carde.className='card';
+	board.appendChild(carde);
+}
 }
